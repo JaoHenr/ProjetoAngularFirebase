aa58cddbd2613e0d6ae2371c260198bf04f33ff4
João Henrique <162513064+JaoHenr@users.noreply.github.com>
2024-08-10 00:29:29 +0000

    Montagem do Curriculo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,6 +10,40 @@ import { MessageService } from '../services/message.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  // variaveis
+  pessoa = {
+    foto: 'https://img.freepik.com/premium-photo/stoic-man-statue-strong-stoic-man_358089-402.jpg',
+    nome: 'Julianus Pastelus',
+    objetivo: 'Programo com o C#',
+    contato: {
+      email: 'E-mail: JoaoHenr@gmai.com',
+      telefone: 'Telefone: (15) 99756-7423',
+      github: 'Github: github.com/basesweb',
+      linkedin: 'LinkedIn: linkedIn.com/basesweb',
+    },
+    softskills: [
+      'Trabalho em grupo',
+      'Comunicação',
+      'Resiliência',
+      'Proatividade',
+      'Liderança',
+      'Criatividade',
+      'Estoico'
+    ],
+    formacao: [
+      {
+      ano_inicio: '2023',
+      ano_termino: '2024',
+      instituicao: 'Etec Sales Gomes',
+      curso: 'Desenvolvimento de Sistema'
+     },
+     {
+      ano_inicio: '2025',
+      ano_termino: '2028',
+      instituicao: 'Fatec - Tatuí',
+      curso: ' Análise e Desenvolvimento de Sistema'
+     }
+    ]
+  }
   constructor(){ }
 }
