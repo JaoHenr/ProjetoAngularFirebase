67a73359c377ddd2382b4b26617d321c5e0a0812
João Henrique <162513064+JaoHenr@users.noreply.github.com>
2024-08-17 01:48:31 +0000

    Diogomia

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,5 +11,79 @@ import { MessageService } from '../services/message.service';
 })
 export class HomePage {
 
-  constructor(){ }
+  segments: string = 'usuarios';
+
+  usuarios =  [
+    {
+      nome: 'Neide',
+      idade: '20',
+      genero: 'Feminino',
+      rotinas: [
+        {
+          dia: 'Segunda-Feira',
+          treinos: [
+            'Rosca Direta',
+            'Cruxifixo',
+            'Remada Sumô'
+          ]
+        },
+        {
+          dia: 'Quarta-Feira',
+          treinos: [
+            'Rosca invertida',
+            'Pulley',
+            'Remada Sentada'
+          ]
+        },
+        {
+          dia: 'Sexta-Feira',
+          treinos: [
+            'Supino Peito',
+            'Bíceps Corda',
+            'Bíceps Hulk Invertido'
+          ]
+        },
+      ]
+    },
+    {
+      
+        nome: 'Diogo',
+        idade: '30',
+        genero: 'Masculino',
+        rotinas: [
+          {
+            dia: 'Terça-Feira',
+            treinos: [
+              'Sumô Monge Budista',
+              'Corrida de Biga',
+              'Barra Homem Aranha'
+            ]
+          },
+          {
+            dia: 'Quinta-Feira',
+            treinos: [
+              'Levantamento do Conan',
+              'Pulo do Mario',
+              'Bíceps Tarzan'
+            ]
+          },
+          {
+            dia: 'Sabado-Feira',
+            treinos: [
+              'Sentada Dupla',
+              'Flexão Romana',
+              'Agachamento Germânico'
+            ]
+          },
+        ]
+    }
+    
+  ]
+
+  constructor(){}
+
+  trocar(event: any){
+    this.segments = event.detail.value;
+  }
+
 }
